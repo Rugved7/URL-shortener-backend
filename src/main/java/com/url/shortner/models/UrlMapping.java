5f3cfd066fb128458059d8f3663002b341fc3eda
Rugved7 <rugvedagasti7@gmail.com>
2025-08-01 16:27:01 +0530

    Entities/Models for Url Shortner added and mapped with relationships

--- src/main/java/com/url/shortner/models/UrlMapping.java
+++ src/main/java/com/url/shortner/models/UrlMapping.java
@@ -1,4 +1,26 @@
 package com.url.shortner.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
 public class UrlMapping {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String originalUrL;
+    private String shortUrl;
+    private int clickCount = 0;
+    private LocalDateTime createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "UrlMapping")
+    private List<ClickEvent> clickEvents;
 }
