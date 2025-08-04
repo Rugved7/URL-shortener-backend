2b5fb710b76e67c396c7f2773e00e4f5b178454e
Rugved7 <rugvedagasti7@gmail.com>
2025-08-04 22:18:53 +0530

    Phase-I of backend done

--- src/main/java/com/url/shortner/service/UrlMappingService.java
+++ src/main/java/com/url/shortner/service/UrlMappingService.java
@@ -98,12 +98,11 @@ public class UrlMappingService {
 
     public UrlMapping getOriginalUrl(String shortUrl) {
         UrlMapping urlMapping = urlMappingRepository.findByShortUrl(shortUrl);
-
         if(urlMapping != null) {
             urlMapping.setClickCount(urlMapping.getClickCount() + 1);
             urlMappingRepository.save(urlMapping);
 
-            //Record click Events
+            // -> Record click Events --> How many times a specific URL is clicked
             ClickEvent clickEvent = new ClickEvent();
             clickEvent.setClickDate(LocalDateTime.now());
             clickEvent.setUrlMapping(urlMapping);
